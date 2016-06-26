41e7f0b866722b85e9216434716c3744e3820f0f
Belova Irina <bis.front-end@ya.ru>
2016-06-26 11:50:13 +0300

    map

--- source/js/app.js
+++ source/js/app.js
@@ -17,3 +17,25 @@
    $(this).attr('placeholder',$(this).data('placeholder'));
  });
  });
+
+/* Загрузка карты */
+function initialize()
+{
+  var mapOptions = {
+    center: new google.maps.LatLng(54.83863299999999,83.09406190000004),
+    // center: new google.maps.LatLng(51.508742,-0.120850),
+    zoom:17,
+    panControl:true,
+    zoomControl:true,
+    mapTypeControl:true,
+    scaleControl:false,
+    streetViewControl:true,
+    overviewMapControl:true,
+    rotateControl:true,
+    scrollwheel: false,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+
+  };
+  var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
